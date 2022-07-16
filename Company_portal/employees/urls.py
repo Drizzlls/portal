12f81b238adbf3ea61b31badafff7c08a3e9c524
Drizzlls <drizzlls@yandex.ru>
2022-07-16 15:10:44 +0300

    Сделал добавление сотрудника в битрикс + страницу отдельную + поправил функцию выгрзуки сотрудников(теперь функция обновляет данные и загружает список всех сотрудников)

--- Company_portal/employees/urls.py
+++ Company_portal/employees/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import UsersFromBitrix
+from .views import *
 
 urlpatterns = [
     path('', UsersFromBitrix.showListUsers,name="ListUsers"),
     path('user/<int:idSotr>',UsersFromBitrix.profile,name="userProfile"),
-    path('load',UsersFromBitrix.loadUsers)
+    path('load',UsersFromBitrix.loadUsers),
+    path('add',addPerson,name='AddPerson'),
+    path('registration',registrationUser,name='RegUser')
 ]
--- Company_portal/employees/views.py
+++ Company_portal/employees/views.py
@@ -4,15 +4,23 @@ from django.http import HttpResponse
 from bitrix24 import *
 from .models import CompanyEmployees
 
+
+
+# Создать класс с методами битрикса(гет,адд и т.д)
 class UsersFromBitrix:
     def generateUsers():
-        webhook = "https://novoedelo.bitrix24.ru/rest/16/3lhovi0rx5bcsqif/"
+        webhook = "https://novoedelo.bitrix24.ru/rest/16/rze2rg3z1cdu9ua7/"
         b = Bitrix24(webhook)
-        getUsers = b.callMethod( "user.get")
+        getUsers = b.callMethod("user.get")
         for user in getUsers:
-            # pprint.pprint(sotrudn)
             try:
-                sot = CompanyEmployees.objects.get(idАFromBitrix=user['ID'])
+                updateData = CompanyEmployees.objects.get(idАFromBitrix=user['ID'])
+                form = updateData(name=f"{user['NAME']}",
+                    surname=f"{user['LAST_NAME']}",
+                    idАFromBitrix=f"{user['ID']}",
+                    position=f"{user['WORK_POSITION']}",
+                    isActive = f"{user['ACTIVE']}")
+                form.save()
             except:
                 form = CompanyEmployees(
                     name=f"{user['NAME']}",
@@ -25,6 +33,11 @@ class UsersFromBitrix:
 
     def loadUsers(request):
         UsersFromBitrix.generateUsers()
+        try:
+            return HttpResponse('Загрузка прошла успешно')
+
+        except:
+            return HttpResponse('Что то пошло не так')
 
     def showListUsers(request):
         users = CompanyEmployees.objects.all()
@@ -33,3 +46,16 @@ class UsersFromBitrix:
     def profile(request,idSotr):
         sotr = CompanyEmployees.objects.get(idАFromBitrix=idSotr)
         return render(request,"employees/profile.html",{"sotr":sotr})
+
+
+def addPerson(request):
+    webhook = "https://novoedelo.bitrix24.ru/rest/16/rze2rg3z1cdu9ua7/"
+    b = Bitrix24(webhook)
+    try:
+        addUser = b.callMethod('user.add',EMAIL=request.POST["EMAIL"],UF_DEPARTMENT=1,LAST_NAME=request.POST["LAST_NAME"],NAME=request.POST["NAME"] )
+        return HttpResponse("Все ок тут")
+    except BitrixError as message:
+        return HttpResponse(message)
+
+def registrationUser(request):
+    return render(request, "employees/registration.html")
